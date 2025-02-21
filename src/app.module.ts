6569f20ce64b643318d0d7a1744c34c0bf97ec6d
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-21 17:07:10 +0100

    Set global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
-import { Module } from "@nestjs/common";
+import { Module, ValidationPipe } from "@nestjs/common";
+import { APP_PIPE } from "@nestjs/core";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AlbumsModule } from "./albums/albums.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import databaseConfig from "./config/database.config";
+import { AppDatasource } from "./config/data-source";
 import { UsersModule } from "./users/users.module";
 
 @Module({
-	imports: [TypeOrmModule.forRoot(databaseConfig()), UsersModule, AlbumsModule],
+	imports: [
+		TypeOrmModule.forRoot(AppDatasource.options),
+		UsersModule,
+		AlbumsModule,
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		{ provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) },
+	],
 })
 export class AppModule {}
