d22567a42f970b6cfcf99e1f4b011b848ef5d3c0
Gjon Hajdari <gjon.hajdari@student.uni-pr.edu>
2025-02-21 15:19:40 +0100

    Create users and albums resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AlbumsModule } from "./albums/albums.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import databaseConfig from "./config/database.config";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-	imports: [TypeOrmModule.forRoot(databaseConfig()), ConfigModule.forRoot()],
+	imports: [TypeOrmModule.forRoot(databaseConfig()), UsersModule, AlbumsModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
